9c369a9c0352e901001be020d1cc5757764b1c50
Maciej Brodowski <maciekbrodowski@gmail.com>
2017-02-27 21:03:27 +0100

    quiz application in 80%

--- app/src/main/java/com/example/rent/sdacoursetools/Quiz/QuizActivity.java
+++ app/src/main/java/com/example/rent/sdacoursetools/Quiz/QuizActivity.java
@@ -1,15 +1,23 @@
 package com.example.rent.sdacoursetools.Quiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.rent.sdacoursetools.R;
+import com.google.gson.Gson;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -21,26 +29,48 @@ import java.io.UnsupportedEncodingException;
  * Created by RENT on 2017-02-25.
  */
 
-public class QuizActivity extends AppCompatActivity{
+public class QuizActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView answer_1_textView;
     private TextView answer_2_textView;
     private TextView answer_3_textView;
     private TextView answer_4_textView;
+    private SingleAnswer firstAnswer;
+    private SingleAnswer secondAnswer;
+    private SingleAnswer thirdAnswer;
+    private SingleAnswer fourthAnswer;
+    private QuizQuestion quizQuestion;
+
+    private static final String INDEX_KEY = "index_key";
+    private int currentQuestionIndex;
+    private QuizContainer quizContainer;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.quiz_activity_main);
 
+        currentQuestionIndex = getIntent().getIntExtra(INDEX_KEY, 0); //cos co nam wystartuje intent to zwroci nam wartosc 0
+
 
         final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progress_bar);
-        ValueAnimator valueAnimator= ObjectAnimator.ofInt(0,100);
+        ValueAnimator valueAnimator = ObjectAnimator.ofInt(0, 100);
         valueAnimator.setDuration(12000);
         valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
                 progressBar.setProgress((Integer) animation.getAnimatedValue());
+            }
+        });
+        valueAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+
+                Intent intent = new Intent(QuizActivity.this, QuizActivity.class);
+                intent.putExtra(INDEX_KEY, ++currentQuestionIndex);
+                startActivity(intent);
+
             }
         });
         valueAnimator.start();
@@ -55,7 +85,90 @@ public class QuizActivity extends AppCompatActivity{
         answer_4_textView = (TextView) findViewById(R.id.fourth);
 
         String json = loadQuizJason();
+        quizContainer = new Gson().fromJson(json, QuizContainer.class);
 
+        quizQuestion = quizContainer.getQuestions().get(currentQuestionIndex);
+
+        questionTextView.setText(quizQuestion.getQuestion());
+
+        firstAnswer = quizQuestion.getAnswers().get(0);
+        secondAnswer = quizQuestion.getAnswers().get(1);
+        thirdAnswer = quizQuestion.getAnswers().get(2);
+        fourthAnswer = quizQuestion.getAnswers().get(3);
+
+        answer_1_textView.setText(firstAnswer.getText());
+        answer_2_textView.setText(secondAnswer.getText());
+        answer_3_textView.setText(thirdAnswer.getText());
+        answer_4_textView.setText(fourthAnswer.getText());
+
+        answer_1_textView.setTag(firstAnswer.isCorrect());
+        answer_2_textView.setTag(secondAnswer.isCorrect());
+        answer_3_textView.setTag(thirdAnswer.isCorrect());
+        answer_4_textView.setTag(fourthAnswer.isCorrect());
+
+        answer_1_textView.setOnClickListener(this);
+        answer_2_textView.setOnClickListener(this);
+        answer_3_textView.setOnClickListener(this);
+        answer_4_textView.setOnClickListener(this);
+
+        playMusic("https://static.mezgrman.de/downloads/wwm/wechsel_nach_stufe_3.mp3"); //na początku pytania
+
+    }
+
+    String url;
+
+    private void playMusic(String url) {
+        this.url = url;
+
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        try {
+            mediaPlayer.setDataSource(url);
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    boolean wasViewedClicked = false;
+
+    @Override
+    public void onClick(View v) {
+        if (!wasViewedClicked) {
+            if ((Boolean) v.getTag()) {
+                Toast.makeText(this, "Odpowiedź poprawna!", Toast.LENGTH_SHORT).show();
+                playMusic("https://static.mezgrman.de/downloads/wwm/richtig_stufe_1.mp3"); //correct answer
+            } else {
+                Toast.makeText(this, "Niestety....zjebałeś", Toast.LENGTH_SHORT).show();
+                playMusic("https://static.mezgrman.de/downloads/wwm/falsch.mp3"); //wrong answer
+            }
+
+            v.postDelayed(new Runnable() {
+
+                @Override
+                public void run() {
+                    if (currentQuestionIndex < quizContainer.getQuestionsCount() - 1) {
+                        Intent intent = new Intent(QuizActivity.this, QuizActivity.class);
+                        intent.putExtra(INDEX_KEY, ++currentQuestionIndex);
+                        startActivity(intent);
+                    } else {
+                        Intent intent = new Intent(QuizActivity.this, QuizActivity.class);
+                        intent.putExtra(INDEX_KEY, 0);
+                        startActivity(intent);
+
+
+                    }
+                }
+
+            }, 3000);
+            wasViewedClicked = true;
+        }
+
+
+//        MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.milionerzy_wrong_answer);
+//        mediaPlayer.start();
 
 
     }
@@ -66,22 +179,20 @@ public class QuizActivity extends AppCompatActivity{
         BufferedReader in = null;
         String str;
         try {
-            json = getAssets().open("assets/quiz_data.json");
+            json = getAssets().open("quiz_data.json");
             in = new BufferedReader(new InputStreamReader(json, "UTF-8"));
 
-            while((str = in.readLine())!= null ) {
-                buf.append(str);}
+            while ((str = in.readLine()) != null) {
+                buf.append(str);
+            }
             in.close();
 
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-       return buf.toString();
+        return buf.toString();
     }
-
-
-
 
 
 //    public void answerClick(View view){
--- app/src/main/java/com/example/rent/sdacoursetools/Quiz/QuizQuestion.java
+++ app/src/main/java/com/example/rent/sdacoursetools/Quiz/QuizQuestion.java
@@ -23,4 +23,5 @@ public class QuizQuestion {
     public List<SingleAnswer> getAnswers() {
         return answers;
     }
+
 }
